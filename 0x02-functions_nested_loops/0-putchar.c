387c4af089bc7f3608d85f52d845ca7a2eaf4cbe
Abdiawol13 <abdiawol88@gmail.com>
2022-12-13 09:28:20 -0800

    Commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints "_putchar"
+ *
+ * Return: On success 1.
+ */
+int main(void)
+{
+	char s[] = "_putchar";
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar(10);
+
+	return (0);
+}
