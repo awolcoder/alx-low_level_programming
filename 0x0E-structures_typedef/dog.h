a268d2d36564bce39cefc706c1f68412b51997ff
awolcoder <abdulbasitawil42@gmail.com>
2025-06-11 08:08:06 -0700

    new commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - represents a dog
+ * @name: name of the dog (string)
+ * @age: age of the dog (float)
+ * @owner: owner of the dog (string)
+ *
+ * Description: A structure to hold basic info about a dog.
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif /* DOG_H */
